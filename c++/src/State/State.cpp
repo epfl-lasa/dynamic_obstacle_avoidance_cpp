1ac29586eaccdddc06a4d1697b20905010803fa9
Baptiste Busch <baptiste.busch@gmail.com>
2019-04-26 09:41:10 +0200

    Correctly initialize linear and angular velocities

--- c++/src/State/State.cpp
+++ c++/src/State/State.cpp
@@ -1,11 +1,13 @@
 #include "DynamicObstacleAvoidance/State/State.hpp"
 
-State::State(const Pose& pose):pose(pose)
+State::State(const Pose& pose):
+pose(pose), linear_velocity(0,0,0), angular_velocity(0,0,0)
 {}
 
-State::State(const Eigen::Vector3f& position):pose(position)
+State::State(const Eigen::Vector3f& position):
+pose(position), linear_velocity(0,0,0), angular_velocity(0,0,0)
 {}
 
 State::State(const Eigen::Vector3f& position, const Eigen::Quaternionf& orientation):
-pose(position, orientation)
+pose(position, orientation), linear_velocity(0,0,0), angular_velocity(0,0,0)
 {}
