a167ddfb798148dedeace90086ec4da8357244fc
Baptiste Busch <baptiste.busch@gmail.com>
2019-05-02 16:39:58 +0200

    Add the PointCloud class structure

---
+++ src/Obstacle/PointCloud.cpp
@@ -0,0 +1,7 @@
+#include "DynamicObstacleAvoidance/Obstacle/PointCloud.hpp"
+
+PointCloud::PointCloud(const std::list<Eigen::Vector3f>& points, const float& safety_margin)
+{}
+
+PointCloud::~PointCloud()
+{}
